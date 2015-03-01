ddd71140c4b26985aa57b03bda26e48a712f1885
kaizhou-sg <kai.zhou.sg@gmail.com>
2015-03-01 09:22:44 +0800

    add

---
+++ first.js
@@ -0,0 +1,32 @@
+// define variables
+// primitive types
+var num1 = 10;
+var num2 = 20;
+var str1 = "hello JavaScript world!";
+var bFlag = true;
+var oNull = null;
+var oUndefined = undefined;
+
+// define functions
+function add(num1, num2){
+	return num1 + num2;
+}
+
+var fAdd = function(num1, num2){
+	return num1 + num2;
+};
+
+// define objects
+var student = new Object();
+// add properties
+student.name = "Zhou Kai";
+student.gender = "male";
+student.age = 29;
+
+// define objects
+var student2 = {
+	"name": "Zhou Kai",
+	"gender": "male";
+	"age": 29
+};
+
